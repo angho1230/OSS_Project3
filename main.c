73603429b6bad5a01cc62c76c9ee090056c558d5
gunminy <gunmin0525@gmail.com>
2023-06-06 19:14:12 +0900

    modified main for argument handling

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "reversi.h"
 #include "net.h"
 
@@ -9,34 +10,64 @@ char messages[7][51];
 int board[8][8];
 
 int main(int argc, char **argv)
-{   
-    int type;
+{
     int con;
-    printf("Welcom! Choose mod(Host: 1, Client: 2, Single-player: 3, local-multi: 4) : ");
-    scanf("%d", &type);
-    if(type == 1){
-        char input[128];
-        printf("What is your port number? ");
-        fflush(stdout);
-        scanf("%s", input);
-        con = host(input);
-    }
-    else if (type == 2)
+    int type;
+    if (argc == 3)
     {
-        char input[2][128];
-        printf("Enter IP you wish to connect \n");
-        fflush(stdout);
-        scanf("%s", input[0]);
-        printf("%s", input[0]);
-        printf("What is your port number? ");
-        fflush(stdout);
-        scanf("%s", input[1]);
-        con = client(input);
+        if (strcmp(argv[1],"-server")==0){
+            con = host(argv[2]);
+            type = 1;
+            reversi_run(type, con);
+        }
+        else printf("ERROR!\n");
+    }
+    else if (argc == 4)
+    {
+        if (strcmp(argv[1],"-client")==0){
+            char input[2][128];
+            strcpy(input[0],argv[2]);
+            strcpy(input[1],argv[3]);
+            con = client(input);
+            reversi_run(type, con);
+        }
+        else printf("ERROR!\n");
+    }
+    else if (argc == 1)
+    {
+        
+        printf("Welcom! Choose mod(Host: 1, Client: 2, Single-player: 3, local-multi: 4) : ");
+        scanf("%d", &type);
+        if (type == 1)
+        {
+            char input[128];
+            printf("What is your port number? ");
+            fflush(stdout);
+            scanf("%s", input);
+            con = host(input);
+            reversi_run(type, con);
+        }
+        else if (type == 2)
+        {
+            char input[2][128];
+            printf("Enter IP you wish to connect \n");
+            fflush(stdout);
+            scanf("%s", input[0]);
+            printf("%s", input[0]);
+            printf("What is your port number? ");
+            fflush(stdout);
+            scanf("%s", input[1]);
+            con = client(input);
+            reversi_run(type, con);
+        }
+        else
+        {
+            printf("ERROR!\n");
+        }
     }
     else{
-        printf("ERROR!");
+        printf("ERROR!\n");
     }
-    reversi_run(type, con);
     return 0;
 }
 
