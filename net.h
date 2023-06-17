880ba848d37ab876b1b1b0d5d4758a2cf7af9d81
angho1230 <angho910@gmail.com>
2023-06-17 21:10:09 +0900

    chat fixed

--- net.h
+++ net.h
@@ -1,6 +1,13 @@
 #define CHAT 1
 #define MOVE 2
 
+#if !defined(SOL_TCP) && defined(IPPROTO_TCP)
+#define SOL_TCP IPPROTO_TCP
+#endif
+#if !defined(TCP_KEEPIDLE) && defined(TCP_KEEPALIVE)
+#define TCP_KEEPIDLE TCP_KEEPALIVE
+#endif
+
 int listen_at_port (int portnum);
 void host_chat (int conn_fd) ;
 int host (char * input) ;
